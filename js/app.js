9add9b55a2f1b649ad5ffdf0a283520945caaac1
Trevor Griswold <Griswold.Trevor@gmail.com>
2019-04-14 21:25:11 -0700

    Made menu hide on small screens when clicked
    Added javascript to bind an even to all top-bar items to collapse the
    top-bar when clicked

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+
+$(document).ready(function($) {
+  $('#responsive-menu li').click(function() { 
+    if (Foundation.MediaQuery.current == 'small'){
+      $('#responsive-menu').css('display', 'none'); 
+    }
+  });
+});
