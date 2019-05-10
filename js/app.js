0216dc3d3cb4e01ab38c2ee0b001f43c59aa4373
Trevor Griswold <Griswold.Trevor@gmail.com>
2019-05-10 15:52:33 -0700

    Large UI Overhaul
    Added background gradient for main content, backgound for screens larger
    than 1200px, divider bars, other misc tweaks

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function($) {
 
 $(document).ready(function($) {
   var timezone = jstz.determine();
-  var pref = '<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=n5ccj3lfblfraspvmk3egpipms%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=';
+  var pref = '<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23e3e9ff&amp;src=n5ccj3lfblfraspvmk3egpipms%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=';
   var suff = '" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>';
   var iframe_html = pref + timezone.name() + suff;
   document.getElementById('eventsCalendar').innerHTML = iframe_html;
