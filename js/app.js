e4525ce5fa7f1eb60eb4b53f508c0378f249960b
Trevor Griswold <Griswold.Trevor@gmail.com>
2019-06-25 11:21:26 -0700

    Added calendar tabs to embed

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(document).ready(function($) {
   var calendar = document.getElementById('eventsCalendar');
   if (calendar) {
     var timezone = jstz.determine();
-    var pref = '<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23e3e9ff&amp;src=n5ccj3lfblfraspvmk3egpipms%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=';
+    var pref = '<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23e3e9ff&amp;src=n5ccj3lfblfraspvmk3egpipms%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=';
     var suff = '" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>';
     var iframe_html = pref + timezone.name() + suff;
     calendar.innerHTML = iframe_html;
